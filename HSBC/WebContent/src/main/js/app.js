33fd0f7232adfe5fc7be798d9936e6a4ca4568f5
hiralal_manmode <hiralal.man@gmail.com>
2016-11-23 01:08:17 +0530

    1. Angular Library Addred 2. Bootstrap library added and used on default.html page 3. Routes are created for page flows 4. ng-include : to insert template inside template
    Change-Id: Id82f538f91a5d51afd0ed90520125c1c9cbf30ec

---
+++ HSBC/WebContent/src/main/js/app.js
@@ -0,0 +1,44 @@
+var gspApp=angular.module('GSPMoveMoney',['ngRoute']);
+
+gspApp.config(['$routeProvider',
+	
+	function($routeProvider)
+	{
+		$routeProvider.
+		
+		when('/dashboard',{
+			templateUrl:"js/html/dashboard.html",
+			controller:''
+			}).
+		
+		when('/login',{
+			templateUrl:'js/html/loginPage.html',
+			controller:''
+		}).	
+			
+		when('/payeemanagement',{
+			templateUrl:'js/html/payeemanagement.html',
+			controller:''
+		}).
+		
+		otherwise({
+			redirectTo :'/dashboard'
+		});
+	}]);
+
+
+gspApp.controller("loginPageController", function($scope)
+{
+	$scope.userName="roshan";
+	$scope.pwd="manmode"
+});
+
+gspApp.controller("dashboardController",function($scope)
+{
+	$scope.msg="This is dashboard page......message from controller";
+});
+
+gspApp.controller("mbController",function($scope)
+{
+	$scope.msg="This is MB page......message from controller";
+});
